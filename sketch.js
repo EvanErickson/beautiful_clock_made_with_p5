8d9a35e0a4ff1200d4f7e58663f8bfdb986b22be
EvanErickson <evanserickson@gmail.com>
2020-02-29 14:28:17 -0800

    rotated clock to correct position

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ function setup() {
 
 function draw() {
     background(0);
+    translate(200, 200);
+    rotate(-90);
 
     let hr = hour();
     let mn = minute();
@@ -15,17 +17,17 @@ function draw() {
     noFill();
     //Second hand
     let sec = map(sc, 0, 60, 0, 360);
-    arc(200, 200, 300, 300, 0, sec);
+    arc(0, 0, 300, 300, 0, sec);
 
     //Minute hand
     stroke(150, 100, 255);
     let min = map(mn, 0, 60, 0, 360);
-    arc(200, 200, 280, 280, 0, min);
+    arc(0, 0, 280, 280, 0, min);
 
     //Hour hand
     stroke(150, 255, 100);
-    let hou = map(hr, 0, 24, 0, 360);
-    arc(200, 200, 260, 260, 0, hou);
+    let hou = map(hr % 12, 0, 12, 0, 360);
+    arc(0, 0, 260, 260, 0, hou);
 
 
 
